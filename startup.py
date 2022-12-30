d1d07861b2d5343ef6f5fea7cf9b97ecf51a09de
Pedro-Ponteiro <silv9797@gmail.com>
2022-12-29 22:03:35 -0300

    Feat: Add ChatPage Class with unread messages collected every five seconds

--- startup.py
+++ startup.py
@@ -1,9 +1,12 @@
 import time
+from datetime import datetime
+from typing import List, Tuple
 
 import chromedriver_autoinstaller
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class QRCodePage:
@@ -21,6 +24,84 @@ class QRCodePage:
         input("Scan it and press ENTER\n->")
 
 
+# TODO: client can create an account for using chatGPT
+
+# TODO: spam detection bans user!
+
+# TODO: maximum 3 messages per minute
+
+
+class Logger:
+    def read_logs():
+        ...
+
+    def add_log():
+        ...
+
+
+class ChatPage:
+    def __init__(self, wd: webdriver.Chrome, robot_name: str) -> None:
+        self.wd = wd
+        self.robot_name = robot_name
+
+    def collect_clients_with_messages(self) -> List[str]:
+        clients = self.wd.find_elements(
+            By.XPATH,
+            "//div[./div[@aria-colindex='2'] and .//span[contains(@aria-label, 'unread message')]]/div[@aria-colindex='2']//span",
+        )
+
+        clients = [client.text for client in clients]
+        return clients
+
+    def enter_client_chat(self, client: str) -> None:
+        searchbar_el = self.wd.find_element(
+            By.XPATH, "//div[@title='Search input textbox']"
+        )
+
+        searchbar_el.clear()
+        searchbar_el.send_keys(client)
+
+        self.wd.find_element(
+            By.XPATH,
+            f"//span[@title='{client}']",
+        ).click()
+
+        searchbar_el.clear()
+
+        self.wd.find_element(By.XPATH, "//span[@data-testid='search']").click()
+        self.wd.find_element(By.XPATH, "//span[@data-icon='x-alt']").click()
+
+    def get_client_messages(self, client: str) -> List[Tuple[str, str]]:
+
+        self.enter_client_chat(client)
+
+        # depois de coletar as mensagens, o robo vai para a conversa "eu mesmo" e limpa o searchbar
+
+        message_root_xpath = (
+            "//div[contains(@class,'message-in')][//span[contains(@class, 'text')]]"
+        )
+
+        message_texts = self.wd.find_elements(
+            By.XPATH, message_root_xpath + "//span[contains(@class, 'text')]/span"
+        )
+        message_texts = [message_text.text for message_text in message_texts]
+
+        message_dates = self.wd.find_elements(
+            By.XPATH, message_root_xpath + "//div[@data-testid='msg-meta']/span"
+        )
+
+        message_dates = [message_date.text for message_date in message_dates]
+
+        message_infos = list(zip(message_texts, message_dates))
+
+        self.enter_client_chat(self.robot_name)
+
+        return message_infos
+
+    def reply_client(self, client: str, reply_message: str) -> None:
+        ...
+
+
 def main():
 
     chromedriver_autoinstaller.install()
@@ -28,6 +109,21 @@ def main():
     with webdriver.Chrome() as wd:
         wd.implicitly_wait(5)
         QRCodePage(wd).wait_manual_QRCode_scan()
+
+        while True:
+            time.sleep(5)
+
+            chatpage = ChatPage(wd, "Eu Mesmo")
+            print("coletando mensagens não lidas...")
+            clients = chatpage.collect_clients_with_messages()
+
+            for client in clients:
+                print(f"Coletando mensagens de {client}...")
+                messages = chatpage.get_client_messages(
+                    "Mariana Prado"
+                )  # TODO: TROCAR ISSO
+
+                print(f"Mensagens de {client}:\n" + str(messages))
 
 
 if __name__ == "__main__":
